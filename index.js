29e56023d1ea1a4c2594dfe986c4a2282a7acfbc
Aditya Kashyap <145245185+ADYAJHA7481@users.noreply.github.com>
2024-08-06 19:10:34 +0530

    basic EJS code

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const figlet = require('figlet');
-figlet("Hello World!!", function (err, data) {
-    if (err) {
-      console.log("Something went wrong...");
-      console.dir(err);
-      return;
-    }
-    console.log(data);
+const express = require("express");
+const app = express();
+
+const port = 8080;
+
+app.set("view engine","ejs");
+
+// app.get("/",(req,res) => {
+//     res.render("home.ejs");
+// })
+app.listen(port,() => {
+    console.log(`listenting on port ${port}`);
+});
+
+// app.get("/rolldice",(req,res) => {
+//     let {diceVal}=Math.floor(Math.random()*6)+1;
+//     res.render("rolldice.ejs",{diceVal});
+// });
+
+app.get("/ig/:username",(req,res)=>{
+    let {username} =req.params;
+    res.render("insta.ejs",{username});
 });
