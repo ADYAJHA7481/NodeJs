10a33b3c156919b0f531e91582fe4a454db71784
Aditya Kashyap <145245185+ADYAJHA7481@users.noreply.github.com>
2024-08-07 18:46:22 +0530

    add some files

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const app = express();
 
 const port = 8080;
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-// app.get("/",(req,res) => {
+// Uncomment if needed
+// app.get("/", (req, res) => {
 //     res.render("home.ejs");
-// })
-app.listen(port,() => {
-    console.log(`listenting on port ${port}`);
-});
-
-// app.get("/rolldice",(req,res) => {
-//     let {diceVal}=Math.floor(Math.random()*6)+1;
-//     res.render("rolldice.ejs",{diceVal});
 // });
 
-app.get("/ig/:username",(req,res)=>{
-    let {username} =req.params;
-    res.render("insta.ejs",{username});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+// Uncomment if needed
+// app.get("/rolldice", (req, res) => {
+//     res.render("rolldice.ejs");
+// });
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    if (data) {
+        res.render("insta.ejs", { data });
+    } else {
+        res.send("User not found");
+    }
 });
