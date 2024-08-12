769e37997481772337305719f6af8b9856f556bc
Aditya Kashyap <145245185+ADYAJHA7481@users.noreply.github.com>
2024-08-12 21:29:56 +0530

    Create_Add_Delete_UniqueId_POST

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
 const express = require("express");
 const app = express();
-
 const port = 8080;
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
-app.set("view engine", "ejs");
 
-// Uncomment if needed
-// app.get("/", (req, res) => {
-//     res.render("home.ejs");
-// });
+app.use(express.urlencoded({extended:true}));
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    {
+        id:uuidv4(),
+        username : "Adya",
+        content : "I love coding"
+    },
+    {
+        id:uuidv4(),
+        username : "Divya",
+        content : "Hard work is important to archeive success"
+    },
+    {
+        id:uuidv4(),
+        username : "Jaya",
+        content : "I love my Family"
+    },
+];
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+})
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+app.post("/posts",(req,res)=>{
+    let {username,content} = req.body;
+    let id = uuidv4();
+    posts.push({id,username,content});
+    res.redirect("/posts");
 });
 
-// Uncomment if needed
-// app.get("/rolldice", (req, res) => {
-//     res.render("rolldice.ejs");
-// });
-
-app.get("/ig/:username", (req, res) => {
-    let { username } = req.params;
-    const instaData = require("./data.json");
-    const data = instaData[username];
-    if (data) {
-        res.render("insta.ejs", { data });
-    } else {
-        res.send("User not found");
-    }
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs",{post})
 });
+
+app.listen(port,() => {
+    console .log(`app is listening on ${port}`);
+})
